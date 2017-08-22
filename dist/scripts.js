7d826fe97616ecf08b2d026f013ebc15f3beb975
Sutangu <sutangu@gmail.com>
2017-08-22 11:29:40 +0600

    updated .gitignore styles upgraded

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,18 +1,18 @@
 'use strict';
 
 var secondHand = document.querySelector('.second-hand');
-var minsHand = document.querySelector('.min-hand');
+var minutesHand = document.querySelector('.min-hand');
 var hourHand = document.querySelector('.hour-hand');
 function setDate() {
     var now = new Date();
     var seconds = now.getSeconds();
     var secondsDegrees = seconds / 60 * 360 + 90;
     secondHand.style.transform = 'rotate(' + secondsDegrees + 'deg)';
-    var mins = now.getMinutes();
-    var minsDegrees = mins / 60 * 360 + seconds / 60 * 6 + 90;
-    minsHand.style.transform = 'rotate(' + minsDegrees + 'deg)';
+    var minutes = now.getMinutes();
+    var minutesDegrees = minutes / 60 * 360 + seconds / 60 * 6 + 90;
+    minutesHand.style.transform = 'rotate(' + minutesDegrees + 'deg)';
     var hour = now.getHours();
-    var hourDegrees = hour / 12 * 360 + mins / 60 * 30 + 90;
+    var hourDegrees = hour / 12 * 360 + minutes / 60 * 30 + 90;
     hourHand.style.transform = 'rotate(' + hourDegrees + 'deg)';
 }
 setInterval(setDate, 1000);
